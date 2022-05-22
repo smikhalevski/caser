9643772e6abe09696f5e594a51f06c88d146de8e
smikhalevski <smikhalevski@gmail.com>
2022-05-22 23:38:25 +0300

    Updated README.md

--- src/main/encodeAlpha.ts
+++ src/main/encodeAlpha.ts
@@ -6,7 +6,7 @@
 const reservedKeywords = new Set('break case catch class const continue debugger default delete do else export extends finally for function if import  in  instanceof  new return super switch this throw try typeof var void while with yield enum implements interface let package private protected public static yield await abstract boolean byte char double final float goto int long native short synchronized throws transient volatile null true false'.split(' '));
 
 /**
- * Encodes index as a valid alpha variable name
+ * Encodes index as a valid alpha variable name.
  *
  * @param index The variable index to encode.
  * @returns A variable name or `undefined` if index encodes as a reserved keyword.
